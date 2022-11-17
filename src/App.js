fe5e28d3184f9fe2e6fb66273635fc360bbe49f2
tobeken <tobik0124@gmail.com>
2022-11-18 02:17:03 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import CartContainer from './components/CartContainer';
+import { useEffect } from 'react';
+import cartItems from './cartItem';
+import { useDispatch, useSelector } from 'react-redux';
+import { calculateTotals } from './features/cart/CartSlice';
+import Modal from './components/Modal';
 
 function App() {
+  const {cartItems} = useSelector((state) => state.cart)
+  const {isOpen } = useSelector((state) => state.modal)
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    dispatch(calculateTotals())
+
+  },[cartItems])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      {isOpen && <Modal />}
+
+      <Navbar />
+      <CartContainer />
+    </main>
   );
 }
 
---
+++ src/components/Modal.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { clearCart } from '../features/cart/CartSlice';
+import { closeModal } from '../features/modal/ModalSlice';
+
+const Modal = () => {
+    const dispatch = useDispatch();
+  return (
+  <aside className='modal-container'>
+    <div className='modal'>
+        <h4>買い物かごをすべてからにしますか？</h4>
+        <div className='btn-container'>
+            <button className='btn confirm-btn' onClick={()=>{
+                dispatch(clearCart())
+                dispatch(closeModal())
+                }}>OK</button>
+            <button className='btn clear-btn' onClick={()=>dispatch(closeModal())}>やめとく</button>
+        </div>
+    </div>
+  </aside>
+  )
+}
+
+export default Modal
